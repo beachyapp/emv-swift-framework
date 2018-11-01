0ec2857754aaac2056a98a039103814fe00ad471
Piotr Ilski <piotr.ilski@gmail.com>
2018-11-01 12:03:12 +0100

    Minor setup changes

---
+++ BeachyEMVReaderControl/BeachyEMVReaderControl.framework/Headers/BeachyEMVReaderControl.h
@@ -0,0 +1,21 @@
+//
+//  BeachyEMVReaderControl.h
+//  BeachyEMVReaderControl
+//
+//  Created by Piotr Ilski on 29/10/2018.
+//  Copyright © 2018 Beachy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <BeachyEMVReaderControl/BeachyEMVIDT.h>
+
+//! Project version number for BeachyEMVReaderControl.
+FOUNDATION_EXPORT double BeachyEMVReaderControlVersionNumber;
+
+//! Project version string for BeachyEMVReaderControl.
+FOUNDATION_EXPORT const unsigned char BeachyEMVReaderControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeachyEMVReaderControl/PublicHeader.h>
+
+
---
+++ BeachyEMVReaderControl/iOS/BeachyEMVReaderControl.framework/Headers/BeachyEMVIDT.h
@@ -0,0 +1,15 @@
+//
+//  BeachyEMVIDT.h
+//  BeachyEMVReaderControl
+//
+//  Created by Piotr Ilski on 29/10/2018.
+//  Copyright © 2018 Beachy. All rights reserved.
+//
+
+#ifndef BeachyEMVIDT_h
+#define BeachyEMVIDT_h
+
+#import <CommonCrypto/CommonCryptor.h>
+//#import <IDTech/IDT_VP3300.h>
+
+#endif /* BeachyEMVIDT_h */
